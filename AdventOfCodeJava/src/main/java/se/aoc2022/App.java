1fcddbfa8a37b7302e0f698c5194abd110ad3f33
toha2102 <toha2102@student.miun.se>
2022-12-03 00:09:23 +0100

    2nd day, part one done

--- AdventOfCodeJava/src/main/java/se/aoc2022/App.java
+++ AdventOfCodeJava/src/main/java/se/aoc2022/App.java
@@ -1,6 +1,7 @@
 package se.aoc2022;
 
 import se.aoc2022.day1.DayOneAgent;
+import se.aoc2022.day2.DayTwoAgent;
 
 import java.util.List;
 
@@ -8,16 +9,31 @@ import java.util.List;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private final static String sep = " " + "*~".repeat(10) + " ";
+
     public static void main( String[] args )
     {
         dayOne();
+        dayTwo();
+    }
+    private static void separate(String text) {
+        System.out.println(sep + text + sep);
     }
 
     private static void dayOne() {
+        separate("DAY ONE");
         List<String> fileContent = Util.getContentFromFile("day1.txt").toList();
         System.out.println(new DayOneAgent().mostCalories(fileContent));
         System.out.println(new DayOneAgent().topThreeCalories(fileContent));
     }
+
+    private static void dayTwo() {
+        separate("DAY TWO");
+        List<String> fileContent = Util.getContentFromFile("day2.txt").toList();
+        int result = new DayTwoAgent.Game(fileContent).play();
+        System.out.printf("The result is %d%n", result);
+    }
+
+
 }
--- AdventOfCodeJava/src/main/java/se/aoc2022/day2/DayTwoAgent.java
+++ AdventOfCodeJava/src/main/java/se/aoc2022/day2/DayTwoAgent.java
@@ -1,4 +1,143 @@
 package se.aoc2022.day2;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DayTwoAgent {
+
+
+    /*
+    Rock        - A & X     - 1pt
+    Paper       - B & Y     - 2pt
+    Scissors    - C & Z     - 3pt
+
+    Opponent    - A, B, C
+    Me          - X, Y, Z
+
+    Loss        - 0pt
+    Draw        - 3pt
+    Win         - 6pt
+
+    For example, suppose you were given the following strategy guide:
+    A Y
+    B X
+    C Z
+    This strategy guide predicts and recommends the following:
+
+    In the first round,
+    your opponent will choose Rock (A),
+    and you should choose Paper (Y).
+    This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
+
+    In the second round,
+    your opponent will choose Paper (B),
+    and you should choose Rock (X).
+    This ends in a loss for you with a score of 1 (1 + 0).
+
+    The third round is a draw with both players choosing Scissors,
+    giving you a score of 3 + 3 = 6.
+    */
+    enum Choices {
+        ROCK(1), PAPER(2), SCISSORS(3);
+        private final int pt;
+        Choices(final int pt) {
+            this.pt = pt;
+        }
+
+        public int getPt() {
+            return pt;
+        }
+
+        public Choices getWeakness() {
+            return switch (this) {
+                case ROCK -> PAPER;
+                case PAPER -> SCISSORS;
+                case SCISSORS -> ROCK;
+            };
+        }
+    }
+
+    public static class Game {
+        Opponent opponent = new Opponent();
+        Player player = new Player();
+        List<String> guide;
+
+        public Game(List<String> guide) {
+            this.guide = guide;
+        }
+
+        public int play() {
+            int score = 0;
+            for (String line : guide) {
+                String[] split = line.split(" ");
+                opponent.setChoice(split[0]);
+                player.setChoice(split[1]);
+                Round round = new Round(opponent, player);
+                score += round.getScore();
+            }
+            return score;
+        }
+
+    }
+
+    public static class Round {
+        private final Opponent opponent;
+        private final Player player;
+
+        public Round(Opponent opponent, Player player) {
+            this.opponent = opponent;
+            this.player = player;
+        }
+
+        public int getScore() {
+            int playerScore = player.getChoice().getPt();
+            if (opponent
+                    .getChoice()
+                    .equals(player.getChoice())) {
+                playerScore += 3;
+            } else if (opponent
+                    .getChoice()
+                    .getWeakness()
+                    .equals(player.getChoice())) {
+                playerScore += 6;
+            }
+            return playerScore;
+        }
+    }
+    public static class Opponent {
+
+        private Choices choice;
+        public Opponent() {
+        }
+
+        public void setChoice(String choice) {
+            switch (choice) {
+                case "A" -> this.choice = Choices.ROCK;
+                case "B" -> this.choice = Choices.PAPER;
+                case "C" -> this.choice = Choices.SCISSORS;
+            }
+        }
+        public Choices getChoice() {
+            return choice;
+        }
+
+    }
+    public static class Player {
+        private Choices choice;
+
+        public Player() {
+        }
+
+        public void setChoice(String choice) {
+            switch (choice) {
+                case "X" -> this.choice = Choices.ROCK;
+                case "Y" -> this.choice = Choices.PAPER;
+                case "Z" -> this.choice = Choices.SCISSORS;
+            }
+        }
+
+        public Choices getChoice() {
+            return choice;
+        }
+    }
 }
