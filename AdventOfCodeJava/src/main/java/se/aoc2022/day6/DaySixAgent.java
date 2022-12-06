2ba11de09e1480c9ecef4c9ce6966a8b53d26fd8
toha2102 <toha2102@student.miun.se>
2022-12-07 00:08:47 +0100

    day6 regex

---
+++ AdventOfCodeJava/src/main/java/se/aoc2022/day6/DaySixAgent.java
@@ -0,0 +1,7 @@
+package se.aoc2022.day6;
+
+public class DaySixAgent {
+//    Regex lookahead: https://stackoverflow.com/questions/2973436/regex-lookahead-lookbehind-and-atomic-groups
+//    Regex https://regex101.com/r/gxVwL0/1 - (.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.) should work
+
+}
